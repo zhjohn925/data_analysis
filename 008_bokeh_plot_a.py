b2c10c4f505991fe78d8d942a6cb7a9fa0882633
zhjohn925 <zhjohn925@gmail.com>
2023-08-31 21:49:11 -0700

    Update 008_bokeh_plot_a.py

--- 008_bokeh_plot_a.py
+++ 008_bokeh_plot_a.py
@@ -8,7 +8,7 @@ output_file("demo.html")
 if option == 1:
     p = figure(width=400, height=400, title='line')
     p.line([1,2,3,4,5], [6,7,8,9,10], line_width=2)
-else :
+elif option == 2 :
     p = figure(title="Boken Example", x_axis_label='X_value', y_axis_label='Y_value')
     x = np.linspace(0, 10, 30)
     y1 = np.sin(x)
@@ -17,5 +17,7 @@ else :
     p.circle(x, x, legend_label="y=x", fill_color="green", size=5)
     p.line(x, y2, legend_label="y=cos", line_width=3, line_color="red")
     p.triangle(x, x**2, color="black", legend_label="T")
+else :
+    pass
 
 show(p)
